98f1f64eac622934de27f5e9ae91c7b998bf4449
Florian <fwfichtner@gmail.com>
2018-05-11 17:41:57 +0200

    Unittest functionality added
    make result a JSON on Monday please

--- unittest_app.py
+++ unittest_app.py
@@ -1,10 +1,55 @@
-import shoeboxlocation
-from unittest import TestCase
+import json
+import dropbox
+import datetime
+from flask import Flask, request, jsonify
 
-wclass TestIntegrations(TestCase):
+app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+filename = app.config["FILE"]
+access_token = app.config["TOKEN"]
+dbx = dropbox.Dropbox(access_token)
+mode = dropbox.files.WriteMode.overwrite
+
+@app.route('/here', methods=['POST'])
+def here_is_shoebox():
+    data = request.data
+    res = dbx.files_upload(data, filename, mode,
+        client_modified=datetime.datetime.now(),
+        mute=True)
+    return ""
+
+@app.route('/where', methods=['GET'])
+def where_is_shoebox():
+    try:
+        md, res = dbx.files_download(filename)
+    except dropbox.exceptions.HttpError as err:
+        print('*** HTTP error', err)
+        return None
+
+    location = json.loads(res.content)
+    return jsonify(location)
+
+
+
+# import shoeboxlocation
+import unittest
+test_json = '{"lon": 12,"lat": 41}'
+
+class TestIntegrations(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_thing(self):
-        response = self.app.post('/where', json='{"lon": 12,"lat": 35}')
-        assert <make your assertion here>
+    def test_post(self):
+        res = self.app.post('/here', json=test_json)
+        self.assertEqual(res.status_code, 200)
+
+    def test_get(self):
+        res = self.app.get('/where')
+        s = res.data
+        result = json.loads(s)
+        print(type(result))
+        print(result)
+        # self.assertEqual(result, json.loads(test_json))
+
+if __name__ == '__main__':
+    unittest.main()
