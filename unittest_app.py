33a14afdc9459fda7fe11b8bd45f82fa631d3d9d
ivodeliefde <ivodeliefde@gmail.com>
2018-05-16 08:43:45 +0200

    added unittest for checking coordinates on where endpoint

--- unittest_app.py
+++ unittest_app.py
@@ -1,8 +1,5 @@
-import os
 import unittest
 import json
-import dropbox
-import datetime
 from shoeboxlocation import app
 
 
@@ -11,36 +8,49 @@ class TestIntegrations(unittest.TestCase):
         """Set up the testclient and prepare testdata"""
         self.app = app.test_client()
         self.test_location = '{"lon": 12.0,"lat": 41.0}'
+        self.test_location_obj = {"lon": 12.0,"lat": 41.0}
         self.test_location_int = '{"lon": 12,"lat": 41}'
         self.test_location_str = '{"lon": "12.0","lat": "41.0"}'
-        self.test_no_json = '12.0,41.0'
+        self.test_tuple = '12.0,41.0'
 
-    def test_post_status(self):
+    def test_here_status(self):
         """Test if the 'here' endpoint is handling incoming post requests."""
         res = self.app.post('/here', data=self.test_location)
         self.assertEqual(res.status_code, 200, msg=res.get_data(as_text=True))
 
-    def test_post_float(self):
+    def test_here_float(self):
         """Test if the 'here' endpoint rejects post requests without float coordinates."""
         res = self.app.post('/here', data=self.test_location_int)
         self.assertEqual(res.status_code, 400, msg=res.get_data(as_text=True))
         res = self.app.post('/here', data=self.test_location_str)
         self.assertEqual(res.status_code, 400, msg=res.get_data(as_text=True))
 
-    def test_post_json(self):
-        """Test if the 'here' endpoint rejects post requests without json post body."""
-        res = self.app.post('/here', data=self.test_no_json)
+    def test_here_tuple(self):
+        """Test if the 'here' endpoint rejects post requests with tuple of coordinates."""
+        res = self.app.post('/here', data=self.test_tuple)
         self.assertEqual(res.status_code, 400, msg=res.get_data(as_text=True))
 
-    def test_post_empty(self):
-        """Test if the 'here' endpoint rejects post requests without json post body."""
+    def test_here_empty(self):
+        """Test if the 'here' endpoint rejects post requests without a post body."""
         res = self.app.post('/here')
         self.assertEqual(res.status_code, 400, msg=res.get_data(as_text=True))
 
-    def test_get_status(self):
+    def test_here_object(self):
+        """Test if the 'here' endpoint rejects post requests without object data."""
+        res = self.app.post('/here', data=self.test_location_obj)
+        self.assertEqual(res.status_code, 400, msg=res.get_data(as_text=True))
+
+    def test_where_status(self):
         """Test if the 'where' endpoint is handling incoming get requests."""
         res = self.app.get('/where')
         self.assertEqual(res.status_code, 200, msg=res.get_data(as_text=True))
+
+    def test_where_coordinates(self):
+        """Test if the 'where' endpoint is returning a json string with coordinates"""
+        res = self.app.get('/where')
+        data = json.loads(res.get_data(as_text=True))
+        self.assertTrue("lat" in data.keys(), msg="No 'lat' coordinate found")
+        self.assertTrue("lon" in data.keys(), msg="No 'lon' coordinate found")
 
 if __name__ == '__main__':
     unittest.main()
